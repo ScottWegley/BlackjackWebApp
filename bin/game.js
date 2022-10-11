12e9f452d087ff0bfdf9f1f2e9612d6f2af11995
Scott Wegley <scbrwegley@protonmail.ch>
2022-10-10 17:52:38 -0700

    verified settings being stored and read, had to do class redclaration cause pages is dumb

---
+++ bin/game.js
@@ -0,0 +1,28 @@
+"use strict";
+class GameSettings {
+    constructor(iDecks = 6, iCash = 2000, iAdmin = true) {
+        this.decks = iDecks;
+        this.cashStart = iCash;
+        this.admin = iAdmin;
+    }
+    toJSON() {
+        return JSON.stringify({ 'decks': this.decks, 'cashStart': this.cashStart, 'admin': this.admin });
+    }
+    update(inSettings) {
+        if (typeof inSettings === 'string') {
+            var temp = JSON.parse(inSettings);
+            this.decks = temp.decks;
+            this.cashStart = temp.cashStart;
+            this.admin = temp.admin;
+        }
+    }
+}
+window.addEventListener('load', () => {
+    startGame();
+    ;
+});
+function startGame() {
+    var settings = new GameSettings();
+    settings.update(sessionStorage.getItem('blackjacksettings'));
+    console.log(settings.toJSON());
+}
---
+++ src/game.ts
@@ -0,0 +1,34 @@
+class GameSettings {
+    decks: number;
+    cashStart: number;
+    admin: boolean;
+
+    constructor(iDecks: number = 6, iCash: number = 2000, iAdmin: boolean = true) {
+        this.decks = iDecks;
+        this.cashStart = iCash;
+        this.admin = iAdmin
+    }
+
+    toJSON(): string {
+        return JSON.stringify({ 'decks': this.decks, 'cashStart': this.cashStart, 'admin': this.admin });
+    }
+
+    update(inSettings: string | null): void {
+        if (typeof inSettings === 'string') {
+            var temp = JSON.parse(inSettings);
+            this.decks = temp.decks;
+            this.cashStart = temp.cashStart;
+            this.admin = temp.admin;
+        }
+    }
+}
+
+window.addEventListener('load', () => {
+    startGame();;
+})
+
+function startGame(): void {
+    var settings = new GameSettings();
+    settings.update(sessionStorage.getItem('blackjacksettings'));
+    console.log(settings.toJSON());
+}
