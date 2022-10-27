f7e9c179899947650aaa4f5f3f2c4c421e9d04e0
scottwegley <scottwegley@gmail.com>
2022-10-27 03:42:54 -0700

    more conditions for showing split option must have enough money, round being started not already split, right after initial deal, two identical cards or two cards worth ten

--- src/game.ts
+++ src/game.ts
@@ -615,7 +615,7 @@ function updateDisplay(): void {
     btnStand.style.display = (roundStarted ? 'inline-block' : 'none');
     btnHit.style.display = (roundStarted ? 'inline-block' : 'none');
     btnDD.style.display = ((roundStarted && currentMoney > currentBets[(+ hm.first)]) ? 'inline-block' : 'none');
-    btnSplit.style.display = ((roundStarted && !playerHand2.enabled) ? 'inline-block' : 'none');
+    btnSplit.style.display = ((currentMoney >= currentBets[0] && roundStarted && !playerHand2.enabled && playerHand1.currentSize == 2 && (playerHand1.cards[0].value == playerHand1.cards[1].value || (new Array('Ten', 'Jack', 'Queen', 'King').includes(playerHand1.cards[0].value) && new Array('Ten', 'Jack', 'Queen', 'King').includes(playerHand1.cards[1].value)))) ? 'inline-block' : 'none');
     btnSurrender.style.display = (roundStarted ? 'inline-block' : 'none');
 
     pDealer.textContent = pPlayer1.textContent = pPlayer2.textContent = '';
